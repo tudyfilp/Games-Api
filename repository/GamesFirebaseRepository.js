bf3d2fbf023c006217ec7e8e4ceb6699cedb11cf
Isabela <isabela.nazare@evozon.com>
2019-08-21 10:26:11 +0300

    refactored

--- repository/GamesFirebaseRepository.js
+++ repository/GamesFirebaseRepository.js
@@ -4,32 +4,36 @@ class GamesFirebaseRepository extends FirebaseRepository {
     constructor(db) {
         super(db, 'games');
     }
+
     async addSession(key) {
-        let str="games/"+key+"/sessions";
-        let documentRef = await this._database.collection(str).add({availablePlaces:4});
-    
+        
+        let path = "games/" + key + "/sessions";
+
+        let documentRef = await this._database.collection(path).add({availablePlaces:4});
+
         return documentRef.id;
     }
 
    async getSession(key,cb) { 
 
-     this._database.collection('games').doc(key).collection('sessions')
+     let path = "games/" + key + "/sessions";
+
+     this._database.collection(path)
                    .where("availablePlaces",">",0)
                    .limit(1)
                    .get()
-                   .then((querySnapshot2) => {
-                       if(querySnapshot2.empty === true) {    
-
-                           this.addSession(key).then((result)=>cb(result));
-                       }
+                   .then((querySnapshot) => {
+                        if(querySnapshot.empty === true) 
+                        {    
+                            this.addSession(key).then((result)=>cb(result));
+                        }
                        else
                        {  
-                           cb(querySnapshot2.docs[0].id) ;
+                           cb(querySnapshot.docs[0].id) ;
                        }
 
-               });
-           
-       }
+                   });
+   }
 
 }
 
