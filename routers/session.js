6a73f0bb820a05c2a5be426b40c74a320be986d7
teodor.filp <teodor.filp@evozon.com>
2019-09-04 11:47:32 +0300

    merged with remote

--- routers/session.js
+++ routers/session.js
@@ -1,8 +1,7 @@
 var router = require('express').Router();
 const hangmanService = require('../service/hangmanService');
 
-router.post('/hangman/getNewSession', hangmanService.getNewSession); 
-
+router.post('/hangman', hangmanService.getNewSession); 
+router.post('/hangman/prepareClientToConnectToGivenSession', hangmanService.prepareClientToConnectToGivenSession);
 router.post('/hangman/removeUserFromSession', hangmanService.removeUserFromSession);
-
 module.exports = router;
